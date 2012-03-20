9b8cdfd7b82bcc8a00e4a58e82ea1d5a97343255
Bnar Baban <bnar.baban@mirendo.se>
2012-03-20 15:18:56 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
+	before_save :create_remember_token
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,4 +20,10 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
+
+	private
+
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
