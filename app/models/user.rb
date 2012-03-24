58c7cb97242bd865169d036137ba9fcf14ca08d2
Bnar Baban <bnar.baban@mirendo.se>
2012-03-24 14:50:52 +0100

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
+
 	has_secure_password
 	before_save :create_remember_token
 
@@ -22,14 +27,25 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 
-	def feed
-    	# This is preliminary. See "Following users" for the full implementation.
-    	Micropost.where("user_id = ?", id)
+    def feed
+        Micropost.from_users_followed_by(self)
+    end
+
+    def following?(other_user)
+        relationships.find_by_followed_id(other_user.id)
+    end
+
+    def follow!(other_user)
+        relationships.create!(followed_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+        relationships.find_by_followed_id(other_user.id).destroy
     end
 
     private
 
     def create_remember_token
-    	self.remember_token = SecureRandom.urlsafe_base64
+        self.remember_token = SecureRandom.urlsafe_base64
     end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -53,22 +53,22 @@ describe "Static pages" do
 
 		click_link "Help"
 		page.should have_selector 'title', text: full_title('Help')
-		
+
 		click_link "Contact"
 		page.should have_selector 'title', text: full_title('Contact')
-		
+
 		click_link "Home"
 		page.should have_selector 'title', text: full_title('')
-		
+
 		click_link "Sign up now!"
 		page.should have_selector 'title', text: full_title('Sign up')
 	end
-	
+
 	describe "for signed-in users" do
 		let(:user) { FactoryGirl.create(:user) }
 		before do
-			FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-			FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+			FactoryGirl.create(:micropost, :user => user, :content => "Lorem")
+			FactoryGirl.create(:micropost, :user => user, :content => "Ipsum")
 			sign_in user
 			visit root_path
 		end
@@ -77,6 +77,17 @@ describe "Static pages" do
 			user.feed.each do |item|
 				page.should have_selector("li##{item.id}", text: item.content)
 			end
+		end
+
+		describe "follower/following counts" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before do
+				other_user.follow!(user)
+				visit root_path
+			end
+
+			it { should have_link("0 following", href: following_user_path(user)) }
+			it { should have_link("1 follower",  href: followers_user_path(user)) }
 		end
 	end
 end
